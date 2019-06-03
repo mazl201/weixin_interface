43feb1d0ae30d7398d399487fe4ef94514c03a35
mazl201 <mazl2010@qq.com>
2019-06-03 21:21:55 +0800

    weixin interface

--- app.js
+++ app.js
@@ -38,4 +38,54 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/**
+ * [开启跨域便于接口访问]
+ */
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); //访问控制允许来源：所有
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); //访问控制允许报头 X-Requested-With: xhr请求
+    res.header('Access-Control-Allow-Metheds', 'PUT, POST, GET, DELETE, OPTIONS'); //访问控制允许方法
+    res.header('X-Powered-By', 'nodejs'); //自定义头信息，表示服务端用nodejs
+    // res.header('Content-Type', 'application/json;charset=utf-8');
+    next();
+});
+//供微信 访问 验证
+/**
+ * [设置验证微信接口配置参数]
+ */
+const config = {
+    token: 'test', //对应测试号接口配置信息里填的token
+    appid: 'wx0d824e1ba72c5a7e', //对应测试号信息里的appID
+    secret: '008ff7ba221a8723f886a30a482c53fd', //对应测试号信息里的appsecret
+    grant_type: 'client_credential' //默认
+};
+
+
+/**
+ * [验证微信接口配置信息，]
+ */
+app.get('/', function(req, res) {
+
+    const token = config.token; //获取配置的token
+    const signature = req.query.signature; //获取微信发送请求参数signature
+    const nonce = req.query.nonce; //获取微信发送请求参数nonce
+    const timestamp = req.query.timestamp; //获取微信发送请求参数timestamp
+
+    const str = [token, timestamp, nonce].sort().join(''); //排序token、timestamp、nonce后转换为组合字符串
+    const sha = sha1(str); //加密组合字符串
+
+    //如果加密组合结果等于微信的请求参数signature，验证通过
+    if (sha === signature) {
+        const echostr = req.query.echostr; //获取微信请求参数echostr
+        res.send(echostr + ''); //正常返回请求参数echostr
+    } else {
+        res.send('验证失败');
+    }
+});
+
+
+app.listen("3001", '106.12.30.238', function() {
+    console.log(`服务器运行在http://106.12.30.238`);
+});
+
 module.exports = app;
